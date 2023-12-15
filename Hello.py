9987c0d7209add0795e9c628b0aa8b3950523ac8
fabih1996 <153489621+fabih1996@users.noreply.github.com>
2023-12-15 11:39:31 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,56 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+import warnings
 import streamlit as st
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
-
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+# Disattiva tutti i warning
+warnings.filterwarnings("ignore")
 
 
-if __name__ == "__main__":
-    run()
+from joblib import load
+model=load("ultimate_model.joblib")
+
+def insert_data(): 
+	genere = st.selectbox('Genere: ',('Maschio', 'Femmina')) #SESSO
+	if genere=='Maschio':
+		sesso=1
+	else:
+		sesso=0
+
+	age = st.slider('Quanti anni hai?', 0, 130, 25) #ETA'
+    
+	press = st.selectbox('Soffri di ipertensione?',('Sì', 'No')) #PRESSIONE
+	if press=='Sì':
+		ipertensione=1
+	else:
+		ipertensione=0
+
+	cuore = st.selectbox('Hai problemi di cuore?',('Sì', 'No')) #CUORE
+	if cuore=='Sì':
+		problemi_di_cuore=1
+	else:
+		problemi_di_cuore=0
+
+	peso = st.slider('Quanti pesi [kg]?', 0, 130, 50) #PESO
+
+	altezza = st.slider('Quanti sei alto [cm]?', 0, 220, 165) #ALTEZZA
+
+	altezza=altezza/100
+	altezza_quadra=altezza**2
+	bmi=peso/altezza_quadra
+
+	glicata = st.slider('Inserisci glicata [%]: ', 0, 15, 5) #GLICATA
+
+	glicemia = st.slider('Inserisci glicemia: ', 0, 500, 90) #GLICEMIA
+
+	return [[sesso,age,ipertensione,problemi_di_cuore,bmi,glicata,glicemia]]
+
+def calcola_prob(dati):
+	if st.button('Calcola probabilità'):
+		st.write(f"Hai una probabilità del {round(model.predict_proba(dati)[0][1],2)*100}% di avere il diabete")
+
+	else:
+	    st.write('Perfavore, inserisci i tuoi dati.')
+		
+def test():
+	dati=insert_data()
+	calcola_prob(dati)
+
+test()
